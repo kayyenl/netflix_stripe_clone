78519020b21c5ce6a610b2d094e86e822c25673c
kayyenl <yenyenlee9@gmail.com>
2022-12-04 13:43:23 +0900

    managed to make the header disappear after scrolling

--- src/HomeScreen.jsx
+++ src/HomeScreen.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function HomeScreen() {
   return (
     <div className='homescreen'>
+     
     </div>
   )
 }
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import NetflixLogo from './images/netflix_logo.png'
 import AvatarLogo from './images/netflix_avatar_1.png'
-
+import { useState, useEffect } from 'react'
 
 
 function Nav() {
+
+  const [show, handleShow] = useState(true)
+
+  function transitionNavBar() {
+    if (window.scrollY > 100) {
+      handleShow(false);
+    } else {
+      handleShow(true)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar)
+    return () => window.removeEventListener("scroll", transitionNavBar)
+  }, [])
+
   return (
-    <div className='nav nav_black'>
+    <div className={`nav ${show && "nav__black"}`}>
       <div className="nav__contents">
         <img src={NetflixLogo} alt="" className='nav__logo'/>
         <img src={AvatarLogo} alt="" className='nav__avatar'/>
