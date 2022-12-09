4f700e6d504256eb42012444d2370cc8c44f1aaf
kayyenl <yenyenlee9@gmail.com>
2022-12-09 21:39:14 +0900

    finally understood url and backticks in react

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -4,7 +4,7 @@ import requests from './request'
 
 function Banner() {
     const [movie, setMovie] = useState([]);
-
+    let baseImage = "https://image.tmdb.org/t/p/original"
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(requests.fetchTrending)
@@ -28,7 +28,7 @@ function Banner() {
     return (
     <header className='banner' style={{
         backgroundSize: "cover",
-        backgroundImage: `url("https://i.guim.co.uk/img/media/3dc4c4a74fc8d14db65ff38c7c4b37c48a056c29/111_0_3334_2000/master/3334.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=56b28ce41f141e1818a3a439db8c4dca")`,
+        backgroundImage: `url(${baseImage}${movie?.backdrop_path})`,
         backgroundPosition: "center center",
     }}>
         <div className="banner__contents">
