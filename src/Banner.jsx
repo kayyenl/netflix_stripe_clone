7db50ecfdece64c4017e6aa4a106863c82dd3959
kayyenl <yenyenlee9@gmail.com>
2022-12-12 13:35:21 +0900

    hello

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -42,7 +42,7 @@ function Banner() {
             <h1 className="banner__description">
                {truncate(
                 movie?.overview,150)}
-            </h1>
+            </h1> 
         </div>
         <div className="banner--fadebottom" />
         
